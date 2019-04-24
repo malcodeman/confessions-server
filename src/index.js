84e87a746fcc1822fccd7c5a7a94f056b9595cba
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2019-04-24 11:48:23 +0200

    Removes web sockets

--- src/index.js
+++ src/index.js
@@ -1,26 +1,11 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import http from "http";
-import WebSocket from "ws";
 
 import posts_routes from "./posts/posts_routes";
 
 const { PORT, MONGODB_URI } = process.env;
-const port = PORT;
 const app = express();
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-
-wss.on("connection", ws => {
-  ws.on("message", message => {
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify(message));
-      }
-    });
-  });
-});
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
@@ -33,4 +18,4 @@ app.use(
 app.use(express.json());
 app.use("/posts", posts_routes);
 
-server.listen(port, () => console.log(`Running on port ${port}`));
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
--- src/posts/posts_model.js
+++ src/posts/posts_model.js
@@ -7,7 +7,7 @@ const PostSchema = new mongoose.Schema({
   },
   date: {
     type: Date,
-    default: Date.now
+    default: Date.now()
   }
 });
 const Post = mongoose.model("Post", PostSchema);
